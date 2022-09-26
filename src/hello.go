58992445170d91b3cfd09eb6534279e1f26c2420
wsm <653431968@qq.com>
2022-09-26 17:02:56 +0800

    初次接触go

--- src/hello.go
+++ src/hello.go
@@ -1,7 +1,33 @@
+// 当前程序的包名
 package main
 
-import "fmt"
+// 导入其他的包
+// import "fmt"
+import (
+    "fmt"
+    // "os"
+    // "io"
+    // "time"
+    // "strings"
+)
 
+// 常量的定义
+const PI = 3.14
+
+// 全局变量的声明与赋值
+var name = "gopher"
+
+// 一般类型声明
+type newType int
+
+// 结构的声明
+type gopher  struct{}
+
+// 接口的声明
+type golang interface{}
+
+// 由 main 函数作为程序入口点启动
 func main() {  
-    fmt.Println("Hello World!你好，世界！")
+    fmt.Println( PI + 1)
+    fmt.Println("hello"+" la");
 }
