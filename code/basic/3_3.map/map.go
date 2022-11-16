3e54e4f713c5c298a298bdeeaa0a112373de9dc8
wsm <shimin.wang@foxmail.com>
2022-11-16 14:51:53 +0800

    map

--- code/basic/3_3.map/map.go
+++ code/basic/3_3.map/map.go
@@ -4,7 +4,7 @@
  * @Author       :
  * @Date         : 2022-10-12 11:58:27
  * @LastEditors  : Please set LastEditors
- * @LastEditTime : 2022-11-09 14:01:47
+ * @LastEditTime : 2022-11-10 14:42:18
  */
 package main
 
@@ -14,22 +14,36 @@ import (
 )
 
 // 语法 var map1 map[keyType]valueType
+// map的声明是不会分配内存的，初始化需要make，分配内存后才能赋值和使用
 func main() {
-	m := make(map[int]string)
+	// map的声明和注意事项
+	var m map[int]string
+	// 在使用map前，需要先make，make的作用就是给map分配数据空间
+	m = make(map[int]string)
 	m[1] = "ok"
 	m[8] = "yes"
+	// 演示删除
 	delete(m, 1)
+	// 当delete指定的key不存在时，删除不会操作，也不会报错
+	delete(m, 10)
 	fmt.Println(m, m[1], m[0])
+	// 如果希望一次性删除所有的key
+	// 1.遍历所有的key，逐一删除
+	// 2.直接make一个新的空间
+	m = make(map[int]string)
+	fmt.Println(m)
 
-	// sm := make([]map[int]string, 5)
-	// for i := range sm {
-	// 	sm[i] = make(map[int]string, 1)
-	// 	sm[i][2] = "ok"
-	// 	fmt.Println(sm[i])
-	// }
-	// fmt.Println(sm)
+	fmt.Println("----------- 案例二 ----------")
+	sm := make([]map[int]string, 5) // 第二种声明方式
+	for i := range sm {
+		sm[i] = make(map[int]string, 1)
+		sm[i][2] = "ok"
+		fmt.Println(sm[i])
+	}
+	fmt.Println(sm)
 
-	m1 := map[int]string{5: "a", 2: "b", 4: "c", 3: "d", 1: "e"}
+	fmt.Println("----------- 案例三 ----------")
+	m1 := map[int]string{5: "a", 2: "b", 4: "c", 3: "d", 1: "e"} // 第三种声明方式
 	fmt.Println(m1)
 
 	m2 := make(map[string]int)
@@ -39,6 +53,7 @@ func main() {
 	}
 	fmt.Println(m2)
 
+	/** 排序 **/
 	s := make([]int, len(m1))
 	i := 0
 	for kk := range m1 {
@@ -47,6 +62,8 @@ func main() {
 	}
 	sort.Ints(s)
 	fmt.Println(s)
+
+	fmt.Println("----------- 案例四 ----------")
 
 	// 判断键值对是否存在
 	m3 := map[string]int{"Shanghai": 10, "Beijing": 20}
--- code/basic/3_3.map/map2.go
+++ code/basic/3_3.map/map2.go
@@ -1,11 +1,3 @@
-/*
- * @Descripttion :
- * @version      :
- * @Author       :
- * @Date         : 2022-11-08 19:48:39
- * @LastEditors  : Please set LastEditors
- * @LastEditTime : 2022-11-09 14:10:09
- */
 /* map类型切片
  * @Descripttion :
  * @version      :
@@ -16,9 +8,12 @@
  */
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-func main() {
+func example1() {
 	// 方式1：通过索引使用切片的map元素
 	items := make([]map[int]string, 5) // map类型的切片
 	for i := range items {             // 遍历给每个切片分配map
@@ -34,4 +29,27 @@ func main() {
 		v[1] = "hello"              // map元素没有得到初始化
 	}
 	fmt.Printf("方式1 - items2:%v\n", items2)
+}
+
+/*
+切片的数据类型如果是map，则我们称为slice of map，map切片，
+这样使用则【 !! map切片个数就可以动态变化 !! 】了
+*/
+func exampleSliceOfMap() {
+	monster := make([]map[string]string, 2) // map类型的切片
+
+	for i := 0; i < 4; i++ {
+		fmt.Println("one")
+		// 使用append动态增加，并实现了扩容
+		monster = append(monster, map[string]string{
+			"name": "t" + strconv.Itoa(i),
+			"age":  "18",
+		})
+	}
+	fmt.Printf("monster = %#v\n cap： %d ，len：%d", monster, cap(monster), len(monster))
+}
+
+func main() {
+	example1()
+	exampleSliceOfMap()
 }
